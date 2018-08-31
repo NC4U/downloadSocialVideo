0858074a8035a15b016b5d57212141a1413b49fd
HieuPham <havenoescape@mail.com>
2018-08-31 16:33:45 +0700

    lastttttttt

--- view/navbar.php
+++ view/navbar.php
@@ -1,5 +1,5 @@
 <nav >
 	<a href="index.php"><img src="../image/youtube-downloader.png" height="70px" width="70px"></a>
-	<a class="navbar-brand" href="index.php"><h2 style="color: #2F4F4F; position: absolute; bottom: 0px"><b>totalvideo.net</b></h2></a>
+	<a class="navbar-brand" href="index.php"><h4 style="color: #2F4F4F; position: absolute; bottom: 0px">totalvideo.net</h4></a>
 	<!-- <a  href="#footer" style="color: #2F4F4F">Contact</a> 	 -->
 </nav>
